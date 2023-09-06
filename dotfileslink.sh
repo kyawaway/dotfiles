9fe097d1ef44356bd6522bdc835691b435791a54
kyawaway <tkmz0216@gmail.com>
2023-09-06 11:18:20 +0900

    tmux

--- dotfileslink.sh
+++ dotfileslink.sh
@@ -2,3 +2,5 @@ ln -sf ~/dotfiles/.emacs.d ~/.emacs.d
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.vim ~/.vim
 ln -sf ~/dotfiles/.config ~/.config
+ln -sf ~/dotfiles/.tmux ~/.tmux
+ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
